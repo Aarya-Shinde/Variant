20fc7e383db8ed3f98a091e960ca9dd0cbe7b962
Aarya Shinde <aaryashinde2020@gmail.com>
2025-04-01 07:39:07 +0530

    testing flexible navbar

---
+++ test/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar">
+    <a href="index.php">Home</a>
+    <a href="novels.php">Novels</a>
+    <a href="about.php">About</a>
+    <a href="contact.php">Contact</a>
+</nav>
+
+<!-- Add your navbar CSS in your main stylesheet -->
+<style>
+    .navbar {
+        background: #2c2c2c;
+        padding: 10px;
+        text-align: center;
+    }
+    .navbar a {
+        color: white;
+        text-decoration: none;
+        margin: 0 15px;
+        font-size: 18px;
+    }
+    .navbar a:hover {
+        color: gold;
+    }
+</style>
