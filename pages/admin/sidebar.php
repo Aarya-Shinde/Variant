12c02575172663a8713bb3df041934dd7ee8a92e
Aarya Shinde <aaryashinde2020@gmail.com>
2026-04-29 20:05:33 +0530

    Created a sidebar component for admin dashboard

---
+++ pages/admin/sidebar.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="../style/admin_dash.css">
+<div class="sidebar">
+    <div class="logo"> 
+        <a href="../index.php">
+            <img src="../images/logowrite.png" alt="Variant Logo" style="max-height: 40px;">
+        </a>
+    </div>
+
+    <ul>
+        <li><a href="admin_dashboard.php">Dashboard</a></li>
+        <li><a href="admin/manage_novels.php">Manage Novels</a></li>
+        <li><a href="admin/all_novels.php">All Novels</a></li>
+        <li><a href="admin/users.php">Users</a></li>
+        <li><a href="admin/reports.php">Reports</a></li>
+        <li><a href="logout.php" class="btn btn-light">Logout</a></li>
+    </ul>
+</div>
